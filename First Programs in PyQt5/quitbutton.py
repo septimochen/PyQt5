8e5954c62067fd65ca8909dc373f15e87b8cdae7
Septimo Chen <qizu.chen@hotmail.com>
2018-05-28 22:58:32 +0800

    modified quit button with center

--- First Programs in PyQt5/quitbutton.py
+++ First Programs in PyQt5/quitbutton.py
@@ -7,7 +7,7 @@ the application terminates.
 
 """
 import sys
-from PyQt5.QtWidgets import QApplication, QPushButton, QWidget
+from PyQt5.QtWidgets import QApplication, QPushButton, QWidget, QDesktopWidget
 from PyQt5.QtGui import QIcon
 
 
@@ -21,13 +21,20 @@ class Example(QWidget):
         qbtn = QPushButton('Quit', self)
         qbtn.clicked.connect(QApplication.instance().quit)
         qbtn.resize(qbtn.sizeHint())
-        qbtn.move(50, 50)
+        qbtn.move(60, 60)
 
         self.setGeometry(300, 300, 250, 150)
         self.setWindowTitle('Dota 2')
         self.setWindowIcon(QIcon('dota_2.png'))
 
+        self.center()
         self.show()
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
 
 
 app = QApplication(sys.argv)
