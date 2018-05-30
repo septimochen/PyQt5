d5d6dc59c0bb4b53299977e1d16e63076e3a904b
Septimo Chen <qizu.chen@hotmail.com>
2018-05-30 10:29:14 +0800

    context menu

---
+++ Menus and toolbars/contextmenu.py
@@ -0,0 +1,30 @@
+import sys
+from PyQt5.QtWidgets import QMainWindow, QMenu, qApp, QApplication
+
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 300, 200)
+        self.setWindowTitle('Context Menu')
+        self.show()
+
+    def contextMenuEvent(self, event):
+
+        cmenu = QMenu(self)
+        newAct = cmenu.addAction('New')
+        opnAct = cmenu.addAction('Open')
+        quitAct = cmenu.addAction('Quit')
+        action = cmenu.exec_(self.mapFromGlobal(event.pos()))
+
+        if action == quitAct:
+            qApp.quit()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
