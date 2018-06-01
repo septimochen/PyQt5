35835ca16739b44f0b71ac45dd5b40484141d90c
Septimo Chen <qizu.chen@hotmail.com>
2018-06-01 22:00:36 +0800

    input dialog

---
+++ Dialogs/inputdialogs.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+ZetCode PyQt5 tutorial
+
+In this example, we receive data from
+a QInputDialog dialog.
+
+"""
+
+import sys
+from PyQt5.QtWidgets import (QWidget, QApplication, QPushButton, QLineEdit, QInputDialog)
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+
+        self.btn = QPushButton('Dialog', self)
+        self.btn.move(50, 50)
+        self.btn.clicked.connect(self.showDialog)
+
+        self.le = QLineEdit(self)
+        self.le.move(130, 22)
+
+        self.setGeometry(500, 500, 500, 400)
+        self.setWindowTitle('Dialog of Dota2')
+        self.show()
+
+    def showDialog(self):
+        text, ok = QInputDialog.getText(self, 'Input Dialog', 'Enter your name')
+
+        if ok:
+            self.le.setText(text)
+
+
+app = QApplication(sys.argv)
+ex = Example()
+sys.exit(app.exec_())
