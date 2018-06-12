46ad16f9e85f1c6f0a98d3e076a7c9415b7bf04e
Septimo Chen <qizu.chen@hotmail.com>
2018-06-12 14:43:06 +0800

    brush 1

--- Painting/brush.py
+++ Painting/brush.py
@@ -5,13 +5,26 @@ import sys
 
 
 class Example(QWidget):
-    def __init__():
+    def __init__(self):
+        super().__init__()
+        self.initUI()
 
-    def initUI():
+    def initUI(self):
+
+        self.setGeometry(300, 300, 480, 320)
+        self.setWindowTitle("QBrush")
+        self.show()
 
     def paintEvent(self, e):
+        qp = QPainter()
+        qp.begin(self)
+        self.drawBrushes(qp)
+        qp.end()
 
     def drawBrushes(self, br):
+        brush = QBrush(Qt.SolidPattern)
+        br.setBrush(brush)
+        br.drawRect(30, 30, 120, 90)
 
 
 if __name__ == '__main__':
