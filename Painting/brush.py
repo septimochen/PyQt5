6628b753553b58410589c349344bb3771a3fb835
Septimo Chen <qizu.chen@hotmail.com>
2018-06-12 14:15:03 +0800

    brush init

---
+++ Painting/brush.py
@@ -0,0 +1,20 @@
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPainter, QBrush
+import sys
+
+
+class Example(QWidget):
+    def __init__():
+
+    def initUI():
+
+    def paintEvent(self, e):
+
+    def drawBrushes(self, br):
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
