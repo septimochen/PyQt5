aa6adbb3640926c888c2e0d206e0edccb02c8eb7
Septimo Chen <qizu.chen@hotmail.com>
2018-06-25 22:50:06 +0800

    beta 2

--- PySide2/Guess-game/guess.py
+++ PySide2/Guess-game/guess.py
@@ -21,21 +21,21 @@ class Ui_Form(object):
         self.text.setGeometry(QtCore.QRect(70, 60, 171, 41))
         self.text.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignTrailing | QtCore.Qt.AlignVCenter)
         self.text.setObjectName("lineEdit")
-        self.label = QtWidgets.QLabel(Form)
-        self.label.setGeometry(QtCore.QRect(80, 150, 151, 31))
-        self.label.setAlignment(QtCore.Qt.AlignCenter)
-        self.label.setObjectName("label")
-        self.lcdNumber = QtWidgets.QLCDNumber(Form)
-        self.lcdNumber.setGeometry(QtCore.QRect(160, 190, 71, 31))
-        self.lcdNumber.display(self.count)
+        self.hintLabel = QtWidgets.QLabel(Form)
+        self.hintLabel.setGeometry(QtCore.QRect(80, 150, 151, 31))
+        self.hintLabel.setAlignment(QtCore.Qt.AlignCenter)
+        self.hintLabel.setObjectName("label")
+        self.lcdCount = QtWidgets.QLCDNumber(Form)
+        self.lcdCount.setGeometry(QtCore.QRect(160, 190, 71, 31))
+        self.lcdCount.display(self.count)
         font = QtGui.QFont()
         font.setFamily("Calibri")
         font.setPointSize(20)
         font.setWeight(75)
         font.setItalic(True)
         font.setBold(True)
-        self.lcdNumber.setFont(font)
-        self.lcdNumber.setObjectName("lcdNumber")
+        self.lcdCount.setFont(font)
+        self.lcdCount.setObjectName("lcdNumber")
         self.label_2 = QtWidgets.QLabel(Form)
         self.label_2.setGeometry(QtCore.QRect(93, 190, 61, 31))
         self.label_2.setObjectName("label_2")
@@ -47,25 +47,25 @@ class Ui_Form(object):
         Form.setWindowTitle(QtWidgets.QApplication.translate("Form", "Form", None, -1))
         self.guessButton.setText(QtWidgets.QApplication.translate("Form", "Guess", None, -1))
         self.text.setText(QtWidgets.QApplication.translate("Form", "0", None, -1))
-        self.label.setText(QtWidgets.QApplication.translate("Form", "<html><head/><body><p><span style=\" font-size:10pt; font-weight:600;\">Please give a try!</span></p></body></html>", None, -1))
+        self.hintLabel.setText(QtWidgets.QApplication.translate("Form", "<html><head/><body><p><span style=\" font-size:10pt; font-weight:600;\">Please give a try!</span></p></body></html>", None, -1))
         self.label_2.setText(QtWidgets.QApplication.translate("Form", "<html><head/><body><p><span style=\" font-weight:600;\">Attempts:</span></p></body></html>", None, -1))
 
     def guess(self):
         self.temp = int(self.text.text())
         #print(self.number)
         self.count += 1
-        self.lcdNumber.display(self.count)
+        self.lcdCount.display(self.count)
 
         if self.temp > self.number:
-            self.label.setText("Too Big!")
+            self.hintLabel.setText("Too Big!")
             self.text.setFocus()
 
         elif self.temp < self.number:
-            self.label.setText("Too Small!")
+            self.hintLabel.setText("Too Small!")
             self.text.setFocus()
 
         else:
-            self.label.setText("Bingo!!!")
+            self.hintLabel.setText("Bingo!!!")
 
 
 if __name__ == "__main__":
