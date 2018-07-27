cfdf1da402f6261d7724b0f7a86a176438c4f149
Septimo Chen <qizu.chen@hotmail.com>
2018-07-27 15:48:52 +0800

    ui initialization

--- PySide2/Estate/main.py
+++ PySide2/Estate/main.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from PySide2 import QtCore, QtGui, QtWidgets
+import sys
 
 
 df = pd.read_csv("data/estatetop100.csv")
@@ -12,4 +14,37 @@ def search(word):
     return ls
 
 
-print(df.loc[df.name.isin(search("万科")), ('rank', 'name')])
+# print(df.loc[df.name.isin(search("万科")), ('rank', 'name')])
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(653, 517)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.tableView = QtWidgets.QTableView(self.centralwidget)
+        self.tableView.setGeometry(QtCore.QRect(40, 170, 581, 321))
+        self.tableView.setObjectName("tableView")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(390, 70, 131, 61))
+        self.pushButton.setObjectName("pushButton")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(80, 70, 271, 61))
+        self.lineEdit.setObjectName("lineEdit")
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QtWidgets.QApplication.translate("MainWindow", "MainWindow", None, -1))
+        self.pushButton.setText(QtWidgets.QApplication.translate("MainWindow", "搜索", None, -1))
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
