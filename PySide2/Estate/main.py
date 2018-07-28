abd3c86705f43ba7e0602ed40eb43cac433d623f
Septimo Chen <qizu.chen@hotmail.com>
2018-07-28 20:37:23 +0800

    updated main

--- PySide2/Estate/main.py
+++ PySide2/Estate/main.py
@@ -6,31 +6,34 @@ import sys
 df = pd.read_csv("data/estatetop100.csv")
 
 
-def search(word):
-    ls = []
-    for company in list(df.name):
-        if word in company:
-            ls.append(company)
-    return ls
-
-
 # print(df.loc[df.name.isin(search("万科")), ('rank', 'name')])
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(653, 517)
+
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.tableView = QtWidgets.QTableView(self.centralwidget)
+        self.tableView = QtWidgets.QLabel(self.centralwidget)
         self.tableView.setGeometry(QtCore.QRect(40, 170, 581, 321))
         self.tableView.setObjectName("tableView")
+        self.ls = []
+
+
+
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton.setGeometry(QtCore.QRect(390, 70, 131, 61))
         self.pushButton.setObjectName("pushButton")
-        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit.setGeometry(QtCore.QRect(80, 70, 271, 61))
-        self.lineEdit.setObjectName("lineEdit")
+        self.pushButton.clicked.connect(self.search)
+
+        self.text = QtWidgets.QLineEdit(self.centralwidget)
+        self.text.setGeometry(QtCore.QRect(80, 70, 271, 61))
+        self.text.setObjectName("lineEdit")
+        self.text.selectAll()
+        self.text.setFocus()
+
+
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
@@ -39,6 +42,16 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QtWidgets.QApplication.translate("MainWindow", "MainWindow", None, -1))
         self.pushButton.setText(QtWidgets.QApplication.translate("MainWindow", "搜索", None, -1))
+
+    def search(self):
+        self.ls = []
+        self.word = self.text.text()
+        for company in list(df.name):
+            if self.word in company:
+                self.ls.append(company)
+
+        # print(df.loc[df.name.isin(ls), ('rank', 'name')])
+        self.tableView.setText(df.loc[df.name.isin(self.ls), ('rank', 'name')].set_index('rank').to_string())
 
 
 if __name__ == "__main__":
